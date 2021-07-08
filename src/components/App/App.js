62b05b9535228c11bee492a54f276a90e4044439
DmitriyZ <zl.dmitriy94@gmail.com>
2021-07-08 19:51:15 +0300

    removed Calculator

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,11 @@
 import css from './App.module.css';
 import NameForm from '../NameForm';
-import Calculator from '../Calculator';
 
 function App() {
 
 return (
   <div className={css.App}>
     <NameForm />
-    <Calculator />
   </div>
 );
 
